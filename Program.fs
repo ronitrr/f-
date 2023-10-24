899681be5381b2a93cd7e4bc9a8801fd50cfa245
ronitrr <114031539+ronitrr@users.noreply.github.com>
2023-10-24 23:04:33 +0100

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,89 @@
+﻿open System
+
+//------------------------write your answer function(s) here---------------------//
+
+// top-level subfunctions of polarToCartesianApprox (if any)
+let makePair x y =
+    (x,y)
+let fact n =
+        if n = 0
+        then 1.0
+        else List.reduce (*) [1.0..float n]
+
+let sinApprox x n =
+    let terms = 
+        [0..n]
+        |> List.map (fun i -> 
+            let term = (x ** float (2*i + 1) / (float (fact (2*i + 1))))
+            if i % 2 = 0 then term else -term // Alternating sign for each term
+        )
+    List.reduce (+) terms
+
+let cosApprox x n =
+    let terms = 
+        [0..n]
+        |> List.map (fun i -> 
+            let term = (x ** float (2*i) / (float (fact (2*i))))
+            if i % 2 = 0 then term else -term // Alternating sign for each term
+        )
+    List.reduce (+) terms
+
+/// answer to Tick1
+// the header given here is correct.
+
+let polarToCartesianApprox (r,theta) n = 
+    let x = float r * (cosApprox theta n)
+    let y = float r * (sinApprox theta n)
+    makePair x y// replace this line with your top-level implementation
+
+
+//--------------------testbench code - DO NOT CHANGE-----------------------------//
+
+/// used to make generate testbench data
+let testInputs =
+    let testPolarCoords = List.allPairs [1.;2.] [1.;2.]
+    List.allPairs testPolarCoords [0;1;2;3;10]
+
+/// data showing correct results generated with model answer and given here
+let testBenchData =
+    [
+        ((1.0, 1.0), 0, (1.0, 0.0))       
+        ((1.0, 2.0), 0, (1.0, 0.0))        
+        ((2.0, 1.0), 0, (2.0, 0.0))        
+        ((2.0, 2.0), 0, (2.0, 0.0))        
+        ((1.0, 1.0), 1, (1.0, 1.0))        
+        ((1.0, 2.0), 1, (1.0, 2.0))        
+        ((2.0, 1.0), 1, (2.0, 2.0))        
+        ((2.0, 2.0), 1, (2.0, 4.0))        
+        ((1.0, 1.0), 2, (0.5, 1.0))        
+        ((1.0, 2.0), 2, (-1.0, 2.0))        
+        ((2.0, 1.0), 2, (1.0, 2.0))        
+        ((2.0, 2.0), 2, (-2.0, 4.0))        
+        ((1.0, 1.0), 3, (0.5, 0.8333333333))        
+        ((1.0, 2.0), 3, (-1.0, 0.6666666667))        
+        ((2.0, 1.0), 3, (1.0, 1.666666667))        
+        ((2.0, 2.0), 3, (-2.0, 1.333333333))        
+        ((1.0, 1.0), 10, (0.5403023038, 0.8414710097))        
+        ((1.0, 2.0), 10, (-0.4161552028, 0.9093474427))        
+        ((2.0, 1.0), 10, (1.080604608, 1.682942019))        
+        ((2.0, 2.0), 10, (-0.8323104056, 1.818694885))
+    ]
+/// test testFun with testData to see whether actual results are the same as
+/// expected results taken from testData
+let testBench testData testFun =
+    let closeTo f1 f2 = abs (f1 - f2) < 0.000001
+    let testItem fn (coords, n, (expectedX,expectedY) as expected) =
+        let actualX,actualY as actual = testFun coords n
+        if not (closeTo actualX expectedX) || not (closeTo actualY expectedY) then
+            printfn "Error: coords=%A, n=%d, expected result=%A, actual result=%A"coords n expected actual
+            1
+        else
+            0
+    printfn "Starting tests..."
+    let numErrors = List.sumBy (testItem testFun) testData
+    printfn "%d tests Passed %d tests failed." (testData.Length - numErrors) numErrors
+
+[<EntryPoint>]
+let main argv =
+    testBench testBenchData polarToCartesianApprox
+    0 // return an integer exit code
